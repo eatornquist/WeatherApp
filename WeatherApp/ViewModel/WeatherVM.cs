ae215f5d663b84780aac5a3d4971585dc1f668e0
eatornquist <eatornquist04@gmail.com>
2022-04-11 10:12:43 -0400

    Binding the ICommand.

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WeatherApp.Model;
+using WeatherApp.ViewModel.Commands;
 using WeatherApp.ViewModel.Helpers;
 
 namespace WeatherApp.ViewModel
@@ -48,6 +49,9 @@ namespace WeatherApp.ViewModel
         }
 
 
+        public SearchCommand SearchCommand { get; set; }
+
+
         public WeatherVM()
         {
             if(DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject())) //If this is true, means that we are currently not running the application and we are currently only designing the application.
@@ -68,6 +72,8 @@ namespace WeatherApp.ViewModel
                    }
                };
             }
+
+            SearchCommand = new SearchCommand(this);
         }
 
         public async void MakeQuery()
